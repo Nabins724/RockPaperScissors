7452c55d3cc0a40cd05ad27a1821ce3d81bc2a66
Nabin Shrestha <nabins724@gmail.com>
2024-08-27 19:15:19 +1000

    game ready to be played for 5 rounds

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RockPaperScissors
+# A game of Rock Paper and Scissor, developed as part of The Odin Project cirriculum.
+# This game will is played in the console so you will need to open console panel in your,
+# browser by opening your developer tools from setting or `ctrl+Shift+I` in the windows.
+
+# The game has certain limitations such as it wont be able to process words other than Rock, Paper and scissor. It is not case sensitive so you are free to type in lower case upper case or mixed.
+
+# Because it cannot process the words other than the specified one in case user enters other words it will be considerd win by the player.
+
--- script.js
+++ script.js
@@ -6,49 +6,72 @@ let computerScore = 0;
 // Computer choice function
 function getComputerChoice() {
     let choice = (Math.random() * 10) / 4;
-    console.log(choice);
+    // console.log(choice);
     if (choice > 2) {
         let computerChoice = "Rock";
-        console.log(computerChoice);
+        console.log("Computer's Choice: " + computerChoice);
         return computerChoice;
     } else if (choice < 2 && choice > 1) {
         let computerChoice = "Paper";
-        console.log(computerChoice);
+        console.log("Computer's Choice: " + computerChoice);
         return computerChoice;
     } else {
         let computerChoice = "Scissor";
-        console.log(computerChoice);
+        console.log("Computer's Choice: " + computerChoice);
         return computerChoice;
     }
-    
+
 }
 
 //Human choice function.
 function getHumanChoice() {
-    let humanChoice = prompt("Please type 'rock' 'paper' 'scissors' to play the game: ");
+    let humanChoice = prompt("Please type 'Rock' || 'Paper' || 'Scissor' to play the game: ");
     let lastString = humanChoice.substring(1);
-    humanChoice = humanChoice[0].toUpperCase()+lastString.toLowerCase();
-    console.log(humanChoice);
+    humanChoice = humanChoice[0].toUpperCase() + lastString.toLowerCase();
+    console.log("Human's Choice: " + humanChoice);
     return humanChoice;
 }
 
 //Function to play a single round
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice){
-        console.log("Draw!!!");
-    }else if(humanChoice === "Rock" && computerChoice === "Paper"){
-        console.log("You Lose! Paper beats Rock.");
-    }else if (humanChoice === "Paper" && computerChoice === "Scissor"){
-        console.log("You Lose! Scissor beats paper.");
-    }else if (humanChoice === "Scissor" && computerChoice === "Rock"){
-        console.log("You lose! Rock beats Scissor!")
-    }else{
-        console.log(`You win! ${humanChoice} beats ${computerChoice} !!!`)
+    if (humanChoice === computerChoice) {
+        console.log(`%c Draw!!!`,`background: grey;`);
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log(`%c You Lose! Paper beats Rock.`,`background: red;`);
+        computerScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        console.log(`%c You Lose! Scissor beats paper.`,`background: red;`);
+        computerScore++;
+    } else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        console.log(`%c You lose! Rock beats Scissor!`,`background: red;`);
+        computerScore++;
+    } else {
+        console.log(`%c You win! ${humanChoice} beats ${computerChoice} !!!`,`background:green;`)
+        humanScore++;
     }
-
+    console.log("Computer's Score: " + computerScore);
+    console.log("Human's Score: " + humanScore);
 }
-//Callin functions
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
+
+// Function to play 5 times.
+function playGame() {
+    for (i = 1; i < 6; i++) {
+        console.log(`%c *****     Round ${i}     *****`, `font-size: 20px; background: Blue;`);
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+
+    }
+    if (humanScore > computerScore) {
+        console.log(`%c Hurray You Won!!! ${humanScore}: ${computerScore}`, `font-size: 25px; background: green;`);
+    } else if (humanScore === computerScore) {
+        console.log(`%c It Was a Draw!!! ${humanScore}: ${computerScore}`, `font-size: 25px; background: orange;`);
+    }
+    else {
+        console.log(`%c Computer Won!!! ${humanScore}: ${computerScore}`, `font-size: 25px; background: red;`);
+    }
+}
+
+//Calling functions
+playGame();
